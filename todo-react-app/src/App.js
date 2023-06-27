0bd5624347f4a42fbd8caa16961a851c2c678816
kyrixhan <kyrix.gh@gmail.com>
2023-06-27 20:27:54 +0900

    CRUE implemented

--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -7,32 +7,44 @@ import './App.css';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    // (1) item -> items 배열로
     this.state = {
-      items: [
-        { id: "0", title: "Hello World 1", done: true },
-        { id: "1", title: "Hello World 2", done: false },
-      ],
+      items: [],
     };
   }
 
+  add = (item) => {
+    const thisItems = this.state.items;
+    item.id = "ID-" + thisItems.length;
+    item.done = false;
+    thisItems.push(item);
+    this.setState({ items: thisItems });
+    console.log("items : " < this.state.items);
+  }
+
+  delete = (item) => {
+    const thisItems = this.state.items;
+    console.log("Before Update Items : ", this.state.items)
+    const newItems = thisItems.filter((e) => e.id !== item.id);
+    this.setState({ items: newItems }, () => {
+      console.log("Update Items : ", this.state.items);
+    });
+  };
+
   render() {
-    // (2) 자바스크립트가 제공하는 map 함수를 이용해 배열을 반복해 <Todo... /> 컴포넌트 생성
     var todoItems = this.state.items.length > 0 && (
       <Paper stype = {{ margin: 16 }}>
         <List>
           { this.state.items.map((item, idx) => (
-            <Todo item = {item} key = {item.id} />
+            <Todo item = {item} key = {item.id} delete = {this.delete} />
           ))}
         </List>
       </Paper>
     );
 
-    // (3) 생성된 컴포넌트 리턴
     return (
       <div className="App">
         <Container maxWidth = "md">
-          <AddTodo />
+          <AddTodo add = {this.add} />
           <div className="TodoList">{todoItems}</div>
         </Container>
       </div>
